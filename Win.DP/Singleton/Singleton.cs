2410c259f25ecf8009ab1f92294b279b3cdf31e7
Kamil <kamilek6@interia.eu>
2017-04-09 15:27:02 +0200

    Implementacja wzorca singleton

---
+++ Win.DP/Singleton/Singleton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /*
+    Singleton to wzorzec strukturalny.
+
+        Cel:
+        Możliwe jest utworzenie tylko jednej instancji danej klasy.
+
+        Implementacja:
+        Ukrycie konstruktora.
+        Metoda umożliwiająca pobranie instancji klasy.
+
+        Uwaga:
+        Należy zabezpieczyć singleton przy dostępie z wielu wątków.
+
+    */
+
+    public class Singleton
+    {
+        //można również od razu utworzyć instację klasy.
+        private static Singleton _singleton;
+
+        private static object lockMultithreading=new object();
+
+        private Singleton()
+        {
+            Console.WriteLine("Tworzę singleton");
+        }
+
+        private List<string> notificationList=new List<string>();
+
+        public void AddNotification(string text)
+        {
+            notificationList.Add(text);
+        }
+
+        public static Singleton Instance()
+        {
+            lock (lockMultithreading)
+            {
+                if (_singleton == null)
+                {
+                    //lazy initialization
+                    _singleton = new Singleton();
+                }
+                return _singleton;
+            }
+        }
+
+    }
+}
---
+++ Win.DP/Win.DP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win.DP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pierwsze pobranie");
+            Singleton.Singleton.Instance().AddNotification("test");
+
+            Console.WriteLine("Drugie pobranie");
+            Singleton.Singleton.Instance().AddNotification("test1");
+
+
+            Console.ReadLine();
+        }
+    }
+}
